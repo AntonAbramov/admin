1c918c0e26be32fad092b5f7f93b878b6a71af32
anton <AntonAbramov1990@gmail.com>
2018-02-05 00:03:53 +0100

    update watches

--- server.js
+++ server.js
@@ -552,6 +552,65 @@ app.put('/macbooksimple/:model/:id', function (req, res) {
 
 // *************** END *****************
 
+// *************** Watch *****************
+
+app.get('/watch/:model', function (req, res) {
+  console.log('I reveived a GET request');
+  var dbName = 'watch' + req.params.model;
+  db[dbName].find().sort({'idx' : 1}, function (err, docs) {
+    if (err) throw err;
+    console.log(docs);
+    res.json(docs);
+  });
+});
+
+app.post('/watch/:model', function (req, res) {
+  console.log(req.body);
+  var dbName = 'watch' + req.params.model;
+  db[dbName].insert(req.body, function (err, doc) {
+    res.json(doc);
+    console.log(doc);
+  })
+
+});
+
+app.delete('/watch/:model/:id', function (req, res) {
+  var id = req.params.id;
+  var dbName = 'watch' + req.params.model;
+  db[dbName].remove({_id: mongojs.ObjectId(id)}, function (err, doc) {
+    res.json(doc);
+  });
+});
+
+app.get('/watch/:model/:id', function (req, res) {
+  var id = req.params.id;
+  var dbName = 'watch' + req.params.model;
+  db[dbName].findOne({_id: mongojs.ObjectId(id)}, function (err, doc){
+    res.json(doc);
+  });
+});
+
+app.put('/watch/:model/:id', function (req, res) {
+  var id = req.params.id;
+  var dbName = 'watch' + req.params.model;
+  db[dbName].findAndModify({query:{_id: mongojs.ObjectId(id)},
+    update: {$set: {
+      name: req.body.name,
+      price: req.body.price,
+      url: req.body.url,
+      description: req.body.description,
+      idx: req.body.idx
+    }
+    },
+    new: true
+
+  }, function (err, doc) {
+    res.json(doc);
+  });
+});
+
+// *************** END *****************
+
 app.listen(5000);
 console.log('Server runing: localhost:5000');
 
